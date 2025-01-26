c60b0180b4bc556c9a5f00492f728fb87b9b853e
Daniel Feliciano-Munoz <danielfelicianomunoz@usf.edu>
2025-01-26 12:54:35 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,9 @@
+//Task 1: Variables
+let employeeName = "John Doe"; //assigning variable employeeName to a string value
+const employeeID = 101; //assigning a constant variable a number value of 101
+var isActive = true; //assigning variable isActive a boolean value of true
+
+//logging each variable's value and type
+console.log("Name: ", employeeName, "Type: ", typeof employeeName); //logging variable employeeName's value and type in the console
+console.log("Number: ", employeeID, "Type: ", typeof employeeID); //logging variable employeeName's value and type in the console
+console.log("Value: ", isActive, "Type: ", typeof isActive); //logging variable employeeName's value and type in the console
