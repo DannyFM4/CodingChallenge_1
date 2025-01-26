263b72ca1c169edb2305a10440233bbb1bac8e8c
Daniel Feliciano-Munoz <danielfelicianomunoz@usf.edu>
2025-01-26 13:23:11 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -5,5 +5,16 @@ var isActive = true; //assigning variable isActive a boolean value of true
 
 //logging each variable's value and type
 console.log("Name: ", employeeName, "Type: ", typeof employeeName); //logging variable employeeName's value and type in the console
-console.log("Number: ", employeeID, "Type: ", typeof employeeID); //logging variable employeeName's value and type in the console
-console.log("Value: ", isActive, "Type: ", typeof isActive); //logging variable employeeName's value and type in the console
+console.log("Number: ", employeeID, "Type: ", typeof employeeID); //logging variable employeeID's value and type in the console
+console.log("Value: ", isActive, "Type: ", typeof isActive); //logging variable isActive value and type in the console
+
+//Task 2: Primitive Data Types
+let productName = "T-Shirt"; //assigning the variable productName a string value of "T-shirt"
+const productPrice = 20; //assigning the constant variable productPrice a number value of 20
+var isAvailable = true; //assigning the variable isAvailable a boolean value of true
+
+//logging each variable's value and type
+console.log("Item: ", productName, "Type: ", typeof productName); //logging variable productName's value and type in the console
+console.log("Cost: ", productPrice, "Type: ", typeof productPrice); //logging variable productPrice's value and type in the console
+console.log("In Stock: ", isAvailable, "Type: ", typeof isAvailable); //logging variable isAvailable's value and type in the console
+
